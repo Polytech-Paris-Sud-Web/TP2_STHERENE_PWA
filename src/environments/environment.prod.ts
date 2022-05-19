57f7572bb19ab1c07397adf310109d457f0bd7aa
stheren <silvain.therene@gmail.com>
2022-05-19 16:06:12 +0200

    [Feat] Change Addr data server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://my-json-server.typicode.com/Polytech-Paris-Sud-Web/TP2_STHERENE_PWA'
+  apiUrl: 'http://www.calenpart.com:3000'
 };
