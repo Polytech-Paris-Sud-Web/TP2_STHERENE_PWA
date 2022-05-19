a871c50d336158c704d6ab8d4f17a2097bd7c10d
stheren <silvain.therene@gmail.com>
2022-05-19 16:26:36 +0200

    [Feat] J ai pas https sur mon serveur, je pleure ...

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://www.calenpart.com:3000'
+  apiUrl: 'https://my-json-server.typicode.com/Polytech-Paris-Sud-Web/TP2_STHERENE_PWA'
 };
